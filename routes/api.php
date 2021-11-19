ea6a2bd01db8713595fc8431f1860ea2c5697ad2
Julian <julian@securedesign.nl>
2021-11-19 12:30:38 +0100

    Make function logout in AuthController and add route

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 });
