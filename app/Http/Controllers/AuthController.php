6f4c30b7a2bc390ba91347340200a41ae1aa44a6
Julian <julian@securedesign.nl>
2021-11-19 12:07:15 +0100

    Make function login in AuthController and add route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -17,7 +19,26 @@ class AuthController extends Controller
       ]);
   }
 
-    public function user(){
-      return 'Authenticated user';
+  public function login(Request $request)
+  {
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return response([
+            'message' => 'Invalid credentials!'
+        ], Response::HTTP_UNAUTHORIZED);
     }
+
+    $user = Auth::user();
+
+    $token = $user->createToken('token')->plainTextToken;
+
+    $cookie = cookie('jwt', $token, 60 * 24); // 1 day
+
+    return response([
+        'message' => 'Success'
+    ])->withCookie($cookie);
+  }
+
+  public function user(){
+    return 'Authenticated user';
+  }
 }
