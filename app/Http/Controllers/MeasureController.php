b63f491f839ab1d477bc779165ce3ba035c42449
Julian <julian@securedesign.nl>
2021-11-29 12:12:14 +0100

    Make MeasureController and add API functions

---
+++ app/Http/Controllers/MeasureController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Measure;
+use Illuminate\Support\Facades\Auth;
+
+class MeasureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $measures = Measure::all();
+
+      return response()->json($measures);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $measure = Measure::findOrFail($id);
+
+      return response()->json($measure);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $measure = Measure::findOrFail($id);
+      $measure->delete();
+
+      return response()->json($measure::all());
+    }
+}
