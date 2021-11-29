816dcad3ccd3600f9848d88af8d712e24753264b
Julian <julian@securedesign.nl>
2021-11-29 11:58:51 +0100

    Create table measures

---
+++ database/migrations/2021_11_29_105633_create_measures_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeasuresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('measures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->string('hand');
+            $table->string('gender');
+            $table->float('hand_lenght');
+
+            $table->timestamps();
+
+            $table->foreign('patient_id')->references('id')->on('patients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('measures');
+    }
+}
