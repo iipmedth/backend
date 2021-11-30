ffe76fccee073d26be9f403c94649a18ea713db0
Julian <julian@securedesign.nl>
2021-11-30 15:16:14 +0100

    Add hand properties to database table measures and Model Measure

--- app/Models/Measure.php
+++ app/Models/Measure.php
@@ -13,6 +13,20 @@ class Measures extends Model
       'patient_id',
       'hand',
       'gender',
+      'hand_length',
+      'hand_breadth',
+      'height_difference_1-2',
+      'height_difference_3-5',
+      'spread_1-2'
+      'spread_1-3'
+      'spread_1-4'
+      'spread_1-5'
+      'spread_2-3'
+      'spread_2-4'
+      'spread_2-5'
+      'spread_3-4'
+      'spread_3-5'
+      'spread_4-5'
     ];
 
     public function patient()
--- database/migrations/2021_11_29_105633_create_measures_table.php
+++ database/migrations/2021_11_29_105633_create_measures_table.php
@@ -18,6 +18,20 @@ class CreateMeasuresTable extends Migration
             $table->unsignedBigInteger('patient_id');
             $table->string('hand');
             $table->string('gender');
+            $table->float('hand_length');
+            $table->float('hand_breadth');
+            $table->float('height_difference_1-3');
+            $table->float('height_difference_3-5');
+            $table->float('spread_1-2');
+            $table->float('spread_1-3');
+            $table->float('spread_1-4');
+            $table->float('spread_1-5');
+            $table->float('spread_2-3');
+            $table->float('spread_2-4');
+            $table->float('spread_2-5');
+            $table->float('spread_3-4');
+            $table->float('spread_3-5');
+            $table->float('spread_4-5');
             $table->timestamps();
 
             $table->foreign('patient_id')->references('id')->on('patients');
@@ -35,7 +49,7 @@ class CreateMeasuresTable extends Migration
         {
             $table->dropForeign('measures_patient_id_foreign');
         });
-        
+
         Schema::dropIfExists('measures');
     }
 }
