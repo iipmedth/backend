0daa808258c3cd858b3ea9621e6b6c6de49b8236
Julian <julian@securedesign.nl>
2021-11-30 17:37:44 +0100

    add App\http\Controllers\MeasureController in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
+use App\http\Controllers\MeasureController;
 
 /*
 |--------------------------------------------------------------------------
