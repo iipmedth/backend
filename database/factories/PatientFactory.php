6a6f05312a867916ca2e313d356573e052d06a04
Julian <julian@securedesign.nl>
2021-12-21 11:09:09 +0100

    Make PatientFactory

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+     public function definition()
+     {
+         return [
+           'user_id' => User::inRandomOrder()->first()->id,
+           'name' => $this->faker->name(),
+           'gender' => 'male',
+           'date_of_birth' => '01-01-1980',
+           'instrument' => 'viool',
+         ];
+     }
+}
