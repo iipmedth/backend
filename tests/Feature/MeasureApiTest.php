3df417af0dbb31e0d822d5454e6054a5e5fee0fb
Julian <julian@securedesign.nl>
2021-12-21 18:43:48 +0100

    Change tests in MeasureApiTest.php

--- tests/Feature/MeasureApiTest.php
+++ tests/Feature/MeasureApiTest.php
@@ -26,7 +26,7 @@ class MeasureApiTest extends TestCase
 
         $this->user = User::factory()->create();
         $this->patient = Patient::factory()->create();
-        $this->measures = Measure::factory()->count(2)->create();
+        $this->measures = Measure::factory()->count(3)->create();
 
         $this->actingAs($this->user);
 
@@ -36,11 +36,27 @@ class MeasureApiTest extends TestCase
 
       public function test_can_show_total_measures()
       {
-         $this->get(route('totalMeasures', ["patient_id"=>$this->measures[0]->patient_id, "hand"=>$this->measures[0]->hand]))->assertStatus(200);
+         $this->get(route('totalMeasures', ["patient_id"=>$this->measures[0]->patient_id, "hand"=>$this->measures[0]->hand]))
+         ->assertStatus(200);
       }
 
       public function test_can_show_percentiles()
       {
-         $this->get(route('percentiles', ["patient_id"=>$this->measures[0]->patient_id, "hand"=>$this->measures[0]->hand]))->assertStatus(200);
+         $this->get(route('percentiles', ["patient_id"=>$this->measures[0]->patient_id, "hand"=>$this->measures[0]->hand]))
+         ->assertStatus(200)
+         ->assertJsonStructure([
+           'height_difference_1_3',
+           'height_difference_3_5',
+           'span_1_2',
+           'span_1_3',
+           'span_1_4',
+           'span_1_5',
+           'span_2_3',
+           'span_2_4',
+           'span_2_5',
+           'span_3_4',
+           'span_3_5',
+           'span_4_5',
+         ]);
       }
 }
