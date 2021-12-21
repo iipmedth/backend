8ee4f799fbeaf7e381bda46fc7216c4a4699e48c
Julian <julian@securedesign.nl>
2021-12-21 13:10:33 +0100

    make setUp() in TherapistOfPatientApiTest.php

---
+++ database/factories/TherapistOfPatientFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Patient;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TherapistOfPatientFactory extends Factory
+{
+  use RefreshDatabase;
+  protected $user;
+  protected $therapist_of_patient;
+  protected $patients;
+  /**
+   * A basic feature test example.
+   *
+   * @return void
+   */
+
+   public function setUp():void{
+     parent::setUp();
+
+     $this->user = User::factory()->create();
+     $this->patients = Patient::factory()->create();
+     $this->therapist_of_patient = TherapistOfPatient::factory()->create();
+
+     $this->actingAs($this->user);
+
+     $this->therapist_of_patient->therapist->save($this->patient);
+     $this->therapist_of_patient->patient->save($this->patient);
+   }
+
+   public function test_can_show_therapist_of_patients()
+   {
+      $this->get(route('measure', ["id"=>$this->patient->id, "hand"=>$this->measure->hand]))->assertStatus(200);
+   }
+
+}
